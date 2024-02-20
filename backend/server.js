58c2a06aaaf09bd82df429281ef0ea550f6aa127
Shobhit <shobhit1256@gmail.com>
2024-02-20 23:35:37 +0530

    deploy issue resolved

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
-const path = require("path")
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+
+
+const path = require("path")
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
@@ -14,7 +17,9 @@ const app = express();
 
 app.use(express.json());
 
-
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 // ----------- DEPLOYMENT ------------------
 
@@ -34,9 +39,7 @@ if (process.env.NODE_ENV === "production") {
 
 // ----------- DEPLOYMENT ------------------
 
-app.use("/api/user", userRoutes);
-app.use("/api/chat", chatRoutes);
-app.use("/api/message", messageRoutes);
+
 
 app.use(notFound);
 app.use(errorHandler);
